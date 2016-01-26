77b59204940c4bd78b47b930ec72aa8e523b3b11
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2016-01-25 23:57:57 -0500

    switch out vocabulary content for photography content

--- app/app.component.js
+++ app/app.component.js
@@ -64,82 +64,49 @@ System.register(['angular2/core'], function(exports_1) {
             // create a var FLASHCARDS of interface type Flashcard, defined above
             FLASHCARDS = [
                 {
-                    question: "Will a higher or lower f/stop create a blurry background?",
-                    answer: "The lower the f/stop — the larger the opening in the lens — the less depth of field — the blurrier the background.",
-                    source: "Nikon - Understanding Maximum Aperture",
-                    sourceUrl: "http://www.nikonusa.com/en/learn-and-explore/article/g3cu6o1r/understanding-maximum-aperture.html"
+                    word: "abate",
+                    definition: "to reduce in amount, degree, or severity",
+                    example: "I hope my hunger abates after eating this granola bar."
                 },
                 {
-                    question: "What is aperture?",
-                    answer: "Aperture refers to the opening of a lens's diaphragm through which light passes. It is calibrated in f/stops and is generally written as numbers such as 1.4, 2, 2.8, 4, 5.6, 8, 11 and 16",
-                    source: "Nikon - Understanding Maximum Aperture",
-                    sourceUrl: "http://www.nikonusa.com/en/learn-and-explore/article/g3cu6o1r/understanding-maximum-aperture.html"
+                    word: "aggrandize",
+                    definition: "to increase in power, influence, and reputation",
+                    example: "The project manager had a tendency to self-aggrandize."
                 },
                 {
-                    question: "How does aperture affect shutter speed?",
-                    answer: "Using a low f/stop means more light is entering the lens and therefore the shutter doesn't need to stay open as long to make a correct exposure which translates into a faster shutter speed.",
-                    source: "Nikon - Understanding Maximum Aperture",
-                    sourceUrl: "http://www.nikonusa.com/en/learn-and-explore/article/g3cu6o1r/understanding-maximum-aperture.html"
+                    word: "erudite",
+                    definition: "scholarly",
+                    example: "A Rhodes Scholar, Max was a true erudite, and a formidable opponent on Jeopardy."
                 },
                 {
-                    question: "What is ISO?",
-                    answer: "For digital photography, ISO refers to the sensitivity—the signal gain—of the camera's sensor.",
-                    source: "Nikon - ISO Control",
-                    sourceUrl: "http://www.nikonusa.com/en/learn-and-explore/article/ga5bvixe/iso-control.html"
+                    word: "auspicious",
+                    definition: "favorable",
+                    example: "The team’s run for the pennant started auspiciously with 24 wins."
                 },
                 {
-                    question: "How is shutter speed measured? ",
-                    answer: "In second fractions: 1/500, 1/250, 1/125, 1/60, 1/30, 1/15, 1/8 etc",
-                    source: "Digital Photography School",
-                    sourceUrl: "http://digital-photography-school.com/shutter-speed/"
+                    word: "sanguine",
+                    definition: "cheerful, optimistic",
+                    example: "A Yale graduate with a 4.0, she was sanguine about finding a job right out of college."
                 },
                 {
-                    question: "How does focal length impact shutter speed?",
-                    answer: "Longer focal lengths will accentuate the amount of camera shake you have and so you’ll need to choose a faster shutter speed. ",
-                    source: "Digital Photography School",
-                    sourceUrl: "http://digital-photography-school.com/shutter-speed/"
+                    word: "mercurial",
+                    definition: "1. Changing one’s personality often and unpredictably. 2. Animated, sprightly",
+                    example: "One never knew exactly what the professor’s class would be like; he was so mercurial that many of his students thought of him as two different people."
                 },
                 {
-                    question: "What's the 'exposure triangle'?",
-                    answer: "ISO, Shutter Speed, Aperture",
-                    source: "Digital Photography School",
-                    sourceUrl: "http://digital-photography-school.com/aperture/"
+                    word: "fastidious",
+                    definition: "nitpicky",
+                    example: "A fastidious eater, Herman would only eat the center of anything he touched."
                 },
                 {
-                    question: "What's the 'S' mode on a camera dial?",
-                    answer: "Shutter priority is very similar to aperture priority mode but is the mode where you select a shutter speed and the camera then chooses all of the other settings",
-                    source: "Digital Photography School",
-                    sourceUrl: "http://digital-photography-school.com/digital-camera-modes/"
+                    word: "reticent",
+                    definition: "tightlipped, not prone to saying much, reluctant",
+                    example: "Paul was reticent and preferred observing others mannerisms."
                 },
                 {
-                    question: "What's a histogram?",
-                    answer: "It graphs the tones in your image from black (on the left) to white (on the right). The higher the graph at any given point the more pixels of that tone that are present in an image. Shoot for an even balance.",
-                    source: "Digital Photography School",
-                    sourceUrl: "http://digital-photography-school.com/understanding-histograms/"
-                },
-                {
-                    question: "What is split lighting in portrait photography?",
-                    answer: "It splits the face exactly into equal halves with one side being in the light, and the other in shadow. To achieve split lighting simply put the light source 90 degrees to the left or right of the subject, and possibly even slightly behind their head.  ",
-                    source: "Digital Photography School",
-                    sourceUrl: "http://digital-photography-school.com/6-portrait-lighting-patterns-every-photographer-should-know/"
-                },
-                {
-                    question: "What is Rembrandt lighting in portrait photography?",
-                    answer: "Rembrandt lighting is identified by the triangle of light on the cheek. To create proper Rembrandt lighting make sure the eye on the shadow side of the face has light in it and has a catch light, otherwise the eye will be “dead” and not have a nice sparkle. ",
-                    source: "Digital Photography School",
-                    sourceUrl: "http://digital-photography-school.com/6-portrait-lighting-patterns-every-photographer-should-know/"
-                },
-                {
-                    question: "What is butterfly lighting in portrait photography?",
-                    answer: "Butterfly lighting is aptly named for the butterfly shaped shadow that is created under the nose by placing the main light source above and directly behind the camera.",
-                    source: "Digital Photography School",
-                    sourceUrl: "http://digital-photography-school.com/6-portrait-lighting-patterns-every-photographer-should-know/"
-                },
-                {
-                    question: "What's the maximum aerial height for drone photography?",
-                    answer: "133 metres (400 feet)",
-                    source: "Digital Photography School",
-                    sourceUrl: "hhttp://digital-photography-school.com/how-to-use-drones-to-do-stunning-aerial-photography/"
+                    word: "egregious",
+                    definition: "standing out in a negative way; shockingly bad",
+                    example: "The dictator’s abuse of human rights was so egregious that many world leaders demanded that he be tried in an international court for genocide."
                 }
             ];
             core_2.enableProdMode();
